038d6f3332fed0df20aafa8fb01915a6ce9d1d03
Bomberus <maximilian@cdsffo.de>
2016-03-31 23:11:14 +0200

    Working Prototype

--- js/blocks.js
+++ js/blocks.js
@@ -1,30 +1,6 @@
 /**
  * Created by Bomberus on 28.03.2016.
  */
-/**
- * @license
- * Visual Blocks Editor
- *
- * Copyright 2012 Google Inc.
- * https://developers.google.com/blockly/
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/**
- * @fileoverview Logic blocks for Blockly.
- * @author q.neutron@gmail.com (Quynh Neutron)
- */
 'use strict';
 
 goog.provide('Blockly.Blocks.input');
@@ -32,37 +8,135 @@ goog.provide('Blockly.Blocks.input');
 goog.require('Blockly.Blocks');
 
 Blockly.Blocks.input.HUE = 210;
-Blockly.Blocks['input'] = {
-    init: function() {
-        this.setColour(210);
-        this.appendDummyInput()
-            .appendField('Leer');
-        this.setOutput(true, 'Type');
-        this.setTooltip('Any type is allowed.');
-        this.setHelpUrl('https://www.youtube.com/watch?v=s2_xaEvcVI0#t=602');
-    }
-};
 
 //Input_Vektor (grad)
-
-//Input_Matrix (grad)
-
-//Input_Textbox (width)
-
-//Input_RangeSlider (min,max,step,[min,max])
-
-//Input_slider (min,max,step,initial)
-
-//Input_color_selector
-
-//Input_checkbox
-
-Blockly.Blocks['input_item'] = {
+Blockly.Blocks['input_vector'] = {
     init: function() {
         this.appendDummyInput()
-            .appendField(new Blockly.FieldVariable("var"), "Bezeichner")
-            .appendField(new Blockly.FieldDropdown([["Nummer", "Nummer"], ["Text", "Text"], ["Funktion", "Funktion"], ["Methode", "Methode"], ["Matrix", "Matrix"], ["Vektor", "Vektor"]]), "type");
-        this.setInputsInline(true);
+            .appendField("Vektor")
+            .appendField(new Blockly.FieldVariable("v"), "Vektor")
+            .appendField("mit")
+            .appendField(new Blockly.FieldTextInput("2"), "grad")
+            .appendField("Feldern");
+        this.appendDummyInput()
+            .appendField("Titel: ")
+            .appendField(new Blockly.FieldTextInput("Titel"), "titel");
+        this.setPreviousStatement(true, "Input");
+        this.setNextStatement(true, "Input");
+        this.setColour(210);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+//Input_Matrix (grad)
+Blockly.Blocks['input_matrix'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("Matrix")
+            .appendField(new Blockly.FieldVariable("m"), "Matrix")
+            .appendField("mit")
+            .appendField(new Blockly.FieldTextInput("3"), "grad")
+            .appendField("Feldern");
+        this.appendDummyInput()
+            .appendField("Titel: ")
+            .appendField(new Blockly.FieldTextInput("Titel"), "titel");
+        this.setPreviousStatement(true, "Input");
+        this.setNextStatement(true, "Input");
+        this.setColour(210);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+//Input_Textbox (width)
+Blockly.Blocks['input_text'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("Textbox")
+            .appendField(new Blockly.FieldVariable("text"), "Textbox")
+            .appendField("mit")
+            .appendField(new Blockly.FieldTextInput("30"), "grad")
+            .appendField("px Weite");
+        this.appendDummyInput()
+            .appendField("Titel: ")
+            .appendField(new Blockly.FieldTextInput("Titel"), "titel");
+        this.setPreviousStatement(true, "Input");
+        this.setNextStatement(true, "Input");
+        this.setColour(210);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+//Input_RangeSlider (min,max,step,[min,max])
+Blockly.Blocks['input_rangeslider'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("Range Slider")
+            .appendField(new Blockly.FieldVariable("rangeslider"), "var");
+        this.appendDummyInput()
+            .appendField("min:")
+            .appendField(new Blockly.FieldTextInput("1"), "min")
+            .appendField("max:")
+            .appendField(new Blockly.FieldTextInput("10"), "max")
+            .appendField("step:")
+            .appendField(new Blockly.FieldTextInput("1"), "step");
+        this.appendDummyInput()
+            .appendField("Titel: ")
+            .appendField(new Blockly.FieldTextInput("Titel"), "titel");
+        this.setPreviousStatement(true, "Input");
+        this.setNextStatement(true, "Input");
+        this.setColour(210);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+//Input_slider (min,max,step,initial)
+Blockly.Blocks['input_slider'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("Slider")
+            .appendField(new Blockly.FieldVariable("slider"), "var");
+        this.appendDummyInput()
+            .appendField("min:")
+            .appendField(new Blockly.FieldTextInput("1"), "min")
+            .appendField("max:")
+            .appendField(new Blockly.FieldTextInput("10"), "max")
+            .appendField("step:")
+            .appendField(new Blockly.FieldTextInput("1"), "step");
+        this.appendDummyInput()
+            .appendField("Titel: ")
+            .appendField(new Blockly.FieldTextInput("Titel"), "titel");
+        this.setPreviousStatement(true, "Input");
+        this.setNextStatement(true, "Input");
+        this.setColour(210);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+//Input_color_selector
+Blockly.Blocks['input_colorbox'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("Colorbox")
+            .appendField(new Blockly.FieldVariable("colorbox"), "var");
+        this.appendDummyInput()
+            .appendField("Titel: ")
+            .appendField(new Blockly.FieldTextInput("Titel"), "titel");
+        this.setPreviousStatement(true, "Input");
+        this.setNextStatement(true, "Input");
+        this.setColour(210);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+//Input_checkbox
+Blockly.Blocks['input_checkbox'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("Checkbox")
+            .appendField(new Blockly.FieldVariable("checkbox"), "var");
+        this.appendDummyInput()
+            .appendField("Titel: ")
+            .appendField(new Blockly.FieldTextInput("Titel"), "titel");
         this.setPreviousStatement(true, "Input");
         this.setNextStatement(true, "Input");
         this.setColour(210);
@@ -78,7 +152,43 @@ Blockly.Blocks['input_list'] = {
             .appendField("Dynamische Eingaben");
         this.setInputsInline(false);
         this.setNextStatement(true, null);
-        this.setColour(210);
+        this.setColour(230);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['statement_list'] = {
+    init: function() {
+        this.appendStatementInput("NAME")
+            .appendField("Ausführung");
+        this.setInputsInline(false);
+        this.setPreviousStatement(true, null);
+        this.setColour(330);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['variable_get'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldVariable("out"), "out");
+        this.setOutput(true, null);
+        this.setTooltip('');
+        this.setColour(20);
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['text_print'] = {
+    init: function() {
+        this.appendValueInput("print")
+            .setCheck(null)
+            .appendField("Print");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(120);
         this.setTooltip('');
         this.setHelpUrl('http://www.example.com/');
     }
--- js/js_blocks.js
+++ js/js_blocks.js
@@ -1,14 +1,109 @@
 /**
  * Created by Bomberus on 28.03.2016.
  */
-Blockly.Python['input'] = function(block) {
-    // Null data type.
-    return 'haloo';
-};
-
 Blockly.Python['input_list'] = function(block) {
     var statements_name = Blockly.Python.statementToCode(block, 'NAME');
     // TODO: Assemble JavaScript into code variable.
-    var code = statements_name;
+    var code = '@interact\n';
+    statements_name = statements_name.replace("  ,","");
+    code += 'def _('+statements_name+'):\n';
+    return code;
+};
+
+Blockly.Python['input_vector'] = function(block) {
+    var text_titel = block.getFieldValue('titel');
+    var variable_vektor = Blockly.Python.variableDB_.getName(block.getFieldValue('Vektor'), Blockly.Variables.NAME_TYPE);
+    var text_grad = block.getFieldValue('grad');
+    // TODO: Assemble Python into code variable.
+    var default_text = 'default=[[';
+    for (var i = 0; i < text_grad; i++) {
+        default_text += i;
+        if (i + 1 < text_grad)
+            default_text += ',';
+    }
+    default_text += ']]';
+        var code = ','+variable_vektor+"=('"+text_titel+"',input_grid(1,"+text_grad+","+default_text+",to_value=lambda x: vector(flatten(x))))";
+    return code;
+};
+
+Blockly.Python['input_matrix'] = function(block) {
+    var text_titel = block.getFieldValue('titel');
+    var variable_matrix = Blockly.Python.variableDB_.getName(block.getFieldValue('Matrix'), Blockly.Variables.NAME_TYPE);
+    var text_grad = block.getFieldValue('grad');
+    // TODO: Assemble Python into code variable.
+    var code = ','+variable_matrix+"=('"+text_titel+"', identity_matrix("+text_grad+"))";
+    return code;
+};
+
+Blockly.Python['input_text'] = function(block) {
+    var text_titel = block.getFieldValue('titel');
+    var variable_textbox = Blockly.Python.variableDB_.getName(block.getFieldValue('Textbox'), Blockly.Variables.NAME_TYPE);
+    var text_grad = block.getFieldValue('grad');
+    // TODO: Assemble Python into code variable.
+    var code = ','+variable_textbox+"=input_box(width="+text_grad+",label='"+text_titel+"')";
+    return code;
+};
+
+Blockly.Python['input_rangeslider'] = function(block) {
+    var text_titel = block.getFieldValue('titel');
+    var variable_var = Blockly.Python.variableDB_.getName(block.getFieldValue('var'), Blockly.Variables.NAME_TYPE);
+    var text_min = block.getFieldValue('min');
+    var text_max = block.getFieldValue('max');
+    var text_step = block.getFieldValue('step');
+    var code = ','+variable_var+"=('"+text_titel+"',range_slider("+text_min+','+
+                                             text_max+','+
+                                             text_step + "))";
+    return code;
+};
+
+Blockly.Python['input_slider'] = function(block) {
+    var text_titel = block.getFieldValue('titel');
+    var variable_var = Blockly.Python.variableDB_.getName(block.getFieldValue('var'), Blockly.Variables.NAME_TYPE);
+    var text_min = block.getFieldValue('min');
+    var text_max = block.getFieldValue('max');
+    var text_step = block.getFieldValue('step');
+    // TODO: Assemble Python into code variable.
+    var code = ','+variable_var+"=('"+text_titel+"',slider("+text_min+','+
+                                       text_max+','+
+                                       text_step + "))";
+    return code;
+};
+
+Blockly.Python['input_colorbox'] = function(block) {
+    var text_titel = block.getFieldValue('titel');
+    var variable_var = Blockly.Python.variableDB_.getName(block.getFieldValue('var'), Blockly.Variables.NAME_TYPE);
+    // TODO: Assemble Python into code variable.
+    var code = ','+variable_var+"=checkbox(True, label='"+text_titel+"')";
+    return code;
+};
+
+Blockly.Python['input_checkbox'] = function(block) {
+    var text_titel = block.getFieldValue('titel');
+    var variable_var = Blockly.Python.variableDB_.getName(block.getFieldValue('var'), Blockly.Variables.NAME_TYPE);
+    // TODO: Assemble Python into code variable.
+    var code = ','+variable_var+"=color_selector(widget='colorpicker', label='"+text_titel+"')";
+    return code;
+};
+
+
+Blockly.Python['statement_list'] = function(block) {
+        var statements_name = Blockly.Python.statementToCode(block, 'NAME');
+        // TODO: Assemble Python into code variable.
+        var code = statements_name;
+        return code;
+    };
+
+Blockly.Python['text_print'] = function(block) {
+    var value_print = Blockly.Python.valueToCode(block, 'print', Blockly.Python.ORDER_ATOMIC);
+    // TODO: Assemble Python into code variable.
+    var code = 'print '+value_print+'\n';
+    return code;
+};
+
+Blockly.Python['variable_get'] = function(block) {
+    var variable_out = Blockly.Python.variableDB_.getName(block.getFieldValue('out'), Blockly.Variables.NAME_TYPE);
+    // TODO: Assemble Python into code variable.
+    var code = variable_out;
+    // TODO: Change ORDER_NONE to the correct strength.
     return code;
 };
